011f1a01d9544e1d3dd2ad2df967382bdc7709e9
Shourya <speedk143@gmail.com>
2025-03-26 20:50:39 +0530

    Created User schema

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: [/\S+@\S+\.\S+/, "Please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving user
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
